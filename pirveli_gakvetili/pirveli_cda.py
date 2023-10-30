0e6a4e0da1dc1accc264501b772f5755a48ebaf8
Meniskuri <giorgikapanadze9169@gmail.com>
2023-10-30 18:07:28 +0400

    gamecheda tvini meore leqciaze var gacherebuli

--- pirveli_gakvetili/pirveli_cda.py
+++ pirveli_gakvetili/pirveli_cda.py
@@ -5,6 +5,16 @@ gio = kursi[0]
 kursi[1]
 print("ra xdeba?")
 print(kursi[0:3])
-help(print)
 
 # (leqcia 2 - sabaziso tipebi cvladebi operaciebi)
+mesiji = "chemi saxelia gio"
+mesiji = mesiji.upper()
+print(mesiji)
+
+sheicavs = "CHEMI" in mesiji
+print(sheicavs)
+
+if sheicavs == True:
+    print("martali var")
+else:
+    print("ar var martali")
