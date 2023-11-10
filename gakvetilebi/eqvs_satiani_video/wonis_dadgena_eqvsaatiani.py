fbd78aaa42332ea60acf98416cd1dae30f1d19f1
Meniskuri <giorgikapanadze9169@gmail.com>
2023-11-10 18:11:11 +0400

    revolution - iwyeba

--- gakvetilebi/eqvs_satiani_video/wonis_dadgena_eqvsaatiani.py
+++ gakvetilebi/eqvs_satiani_video/wonis_dadgena_eqvsaatiani.py
@@ -1,5 +1,5 @@
 
-ramdeni_kilo_xar = int(input("ramdeni kilo xar: "))
+ramdeni_kilo_xar = float(input("ramdeni kilo xar: "))
 wona_in_kg       = ramdeni_kilo_xar
 pounds           = ramdeni_kilo_xar / 0.453
-print(pounds)
+print("kg: ",wona_in_kg,"; pounds: ",pounds)
