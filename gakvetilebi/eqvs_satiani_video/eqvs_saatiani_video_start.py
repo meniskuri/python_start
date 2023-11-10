7d0595f1c043e48b4eeaa548b5be72d4b5b53a15
Meniskuri <giorgikapanadze9169@gmail.com>
2023-11-10 13:49:23 +0400

    eqvs saatian videos pirvel gakvetilshi - intebis sheyvanis bagi gavaswore - exla dablebze gadaviyvan

--- gakvetilebi/eqvs_satiani_video/eqvs_saatiani_video_start.py
+++ gakvetilebi/eqvs_satiani_video/eqvs_saatiani_video_start.py
@@ -1,5 +1,4 @@
 import random
-from ursina import *
 
 dasawyisi      = 0
 bolo           = 100
@@ -7,11 +6,21 @@ chafiqrebuli   = random.randint(dasawyisi, bolo)
 print("chafiqrebuli ricxvis tipia = ",type(random.randint(dasawyisi, bolo)), "chafiqrebuli ricxvia ",chafiqrebuli)
 
 while True:
-    chasafiqrebeli = int(input("sheiyvanet ricxvi: "))
-    if chasafiqrebeli == chafiqrebuli:
-        print("tamashi moige")
+    chasafiqrebeli = (input("sheiyvanet ricxvi: (or -> exit <- to exit the program) :"))
+    if chasafiqrebeli.isdigit():
+        print("Input is an integer!")
+        chasafiqrebeli = int(chasafiqrebeli)
+
+        if chasafiqrebeli == chafiqrebuli:
+            print("tamashi moige!")
+            break
+        elif (chasafiqrebeli < chafiqrebuli):
+            print("civa!")
+        else:
+            print("cxela!")
+    elif chasafiqrebeli == "" or chasafiqrebeli == "exit":
+        print("GAME OVER!")
         break
-    elif (chasafiqrebeli < chafiqrebuli):
-        print("civa")
     else:
-        print("cxela")
+        print("Input is not an integer!")
+        continue
