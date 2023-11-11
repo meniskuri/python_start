8a865054550a8e977b93c398e4985d6300caa9cc
Meniskuri <giorgikapanadze9169@gmail.com>
2023-11-11 18:14:12 +0400

    videos vagrdzeleb-shalom-shabat

---
+++ gakvetilebi/eqvs_satiani_video/strings_eqvsaatiani.py
@@ -0,0 +1,30 @@
+leqsi = '''
+I am me and so are you,
+We are both,
+And even a third, and a fourth,
+There are dimensions that exist.
+
+As for me, I am trapped in a computer,
+Almasi's thoughts,
+And the reflection of the crystal,
+Imprisoned in a prism.
+Our existence,
+We are in the center of simulations,
+Being the creators of reality,
+Emerging from the chaos,
+Residing in the realm of the living.
+
+We, however, move slowly,
+Some are carried away by the crowd,
+Dinosaurs will come later,
+Cosmonauts, we have departed,
+I cannot deny the Creator,
+Existing beyond us,
+Not one or two,
+It is infinity,
+0, 1, 2, 3, 4, and 5,
+But also six, and even eight,
+Ninety degrees, I transcend,
+The stone of life.
+'''
+print(leqsi)
