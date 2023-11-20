583134085d6798ada35b505ad2bf0fb836c49250
Meniskuri <giorgikapanadze9169@gmail.com>
2023-11-20 17:17:54 +0400

    vagrdzeleb 6 saatian videos - tutorial python

---
+++ gakvetilebi/eqvs_satiani_video/chashenebuli_funqcieb.py
@@ -0,0 +1,25 @@
+for x in range(2):
+    print("xxxxxxxxxx")
+    for y in range(1):
+        print("xx")
+print('''xx
+
+''')
+
+
+
+numbers = [5,2,5,2,2]
+i = 0
+for x in numbers:
+    print("x" * int(numbers[i]))
+    i = i + 1
+
+for x in numbers:
+    gamosavali = ""
+    for y in range(x):
+        gamosavali = gamosavali + "y"
+    print(gamosavali)
+
+
+saxelebi = ["gio","vaso","levaniko","luka","teo","azo"]
+print(saxelebi[0])
