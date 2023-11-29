b1d6e977d20ca5b0151f0e84ac4faf1056dc0e9e
Meniskuri <giorgikapanadze9169@gmail.com>
2023-11-29 17:17:56 +0400

    tuples da dicts - rato michirs ese magrad chajdoma?

---
+++ gakvetilebi/eqvs_satiani_video/dicts_eqvsaatiani.py
@@ -0,0 +1,12 @@
+gadamyvani = {
+  "1":"erti",
+  "2":"ori",
+  "3":"sami",
+  "4":"otxi",
+  }
+
+output = ""
+
+while True:
+    phone = input("Phone: ")
+    print(gadamyvani.get(phone))
---
+++ gakvetilebi/eqvs_satiani_video/tupels_eqvsaatiani.py
@@ -0,0 +1,7 @@
+numbers = (1, 3, 5)
+
+#unpacking
+x,y,z = numbers
+print(x)
+print(y)
+print(z)
