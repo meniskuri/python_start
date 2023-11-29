6d9ee738ff1fd74a84b9415a2a65426212ab92dc
Meniskuri <giorgikapanadze9169@gmail.com>
2023-11-29 16:32:15 +0400

    amovyare ertnairebi - michirs dzaan

--- gakvetilebi/eqvs_satiani_video/matrixs_2D_list.py
+++ gakvetilebi/eqvs_satiani_video/matrixs_2D_list.py
@@ -14,3 +14,25 @@ for i in matrix:
         print("matrix[p][k] =                ",matrix[p][k] + 4)
         k = k + 1
     p = p + 1
+
+
+
+print("...........................v2.................................")
+for row in matrix:
+    print(row)
+    for item in row:
+        print(item)
+
+
+
+
+
+print("...... list idan unda amovigo ganmeorebadi ricxvebi ......")
+numbers = [1, 3, 4, 5, 5, 5, 6, 5, 6, 6, 7, 0, 10, 11, 10, 11]
+numbers_chek = []
+
+for i in numbers:
+    if i not in numbers_chek:
+        numbers_chek.append(i)
+
+print(numbers_chek)
