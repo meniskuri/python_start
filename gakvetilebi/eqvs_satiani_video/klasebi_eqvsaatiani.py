192243023750b9bed2bde2a677cbd9181e895583
Meniskuri <giorgikapanadze9169@gmail.com>
2023-11-30 17:19:37 +0400

    rogorc chans momiwevs classebit obieqtebit misi atributebit da konstruqtorebit snake is dawera pyhtonzec

--- gakvetilebi/eqvs_satiani_video/klasebi_eqvsaatiani.py
+++ gakvetilebi/eqvs_satiani_video/klasebi_eqvsaatiani.py
@@ -1,10 +1,21 @@
+# rogorc chans classebit obieqtebit da konstruqtorebit
+# momiwevs pythonzec snake is dawerac. grr... fuck 
+
 class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
     def move(self):
         print("move")
+        self.x = self.x + 1
 
     def draw(self):
         print("draw")
 
+sheviyvanot_x = 4
 
-point1 = Point()
-point1.draw()
+point1 = Point(sheviyvanot_x,3)
+point1.move()
+#point1.x = 1
+print(point1.x)
