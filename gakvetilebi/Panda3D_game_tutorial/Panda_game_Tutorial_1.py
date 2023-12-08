6f0a6bfdc346c4984c3d3837f927d097330b5f73
Meniskuri <giorgikapanadze9169@gmail.com>
2023-12-08 20:06:37 +0400

    panda3D ze gavichede - sxva magaliti minda - videobi unda gavagrdzelo

---
+++ gakvetilebi/Panda3D_game_tutorial/Panda_game_Tutorial_1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+#####################################################
+#                                                   #
+# Original code copyright (c) 2019 Ian Eborn.       #
+# http://thaumaturge-art.com                        #
+#                                                   #
+# Licensed under the MIT license.                   #
+# See "FinalGame/codeLicense".txt, or               #
+# https://opensource.org/licenses/MIT               #
+#                                                   #
+#####################################################
+
+from direct.showbase.ShowBase import ShowBase
+
+from direct.actor.Actor import Actor
+from panda3d.core import WindowProperties
+
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        self.disableMouse()
+
+        properties = WindowProperties()
+        properties.setSize(1000, 750)
+        self.win.requestProperties(properties)
+
+
+
+game = Game()
+game.run()
